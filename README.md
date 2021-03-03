6e7487e0b12af2bcc5071bfdb067ba8918b6a53d
Ashwin A Nair <ashwin.nair@joyent.com>
2021-03-03 15:33:09 -0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# rest-api-graphql
+# REST API Tutorial
+
+This sample is published as part of [the corresponding article](https://www.toptal.com/nodejs/secure-rest-api-in-nodejs) at the Toptal Engineering Blog. Visit https://www.toptal.com/developers/blog and subscribe to our newsletter to read great posts!
+
+## Before using
+
+- Please make sure that you have:
+ - Node.js installed (https://nodejs.org/)
+ - MongoDB installed and running locally (https://www.mongodb.com/)
+   - Using Windows, just open the terminal at where you installed mongo and run `mongod.exe`
+ - Run `npm install` or `yarn` in your root project folder
+
+## Usage
+
+To run the project, please use a command line the following:
+ - `npm start`
+    - It will run the server at port 3600.
---
+++ users/models/users.model.js
@@ -0,0 +1,78 @@
+const mongoose = require('../../common/services/mongoose.service').mongoose;
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    permissionLevel: Number
+});
+
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialised.
+userSchema.set('toJSON', {
+    virtuals: true
+});
+
+userSchema.findById = function (cb) {
+    return this.model('Users').find({id: this.id}, cb);
+};
+
+const userModel = mongoose.model('Users', userSchema);
+
+
+exports.findByEmail = (email) => {
+    return userModel.find({email: email});
+};
+exports.findById = (id) => {
+    return userModel.findById(id)
+        .then((result) => {
+            result = result.toJSON();
+            delete result._id;
+            delete result.__v;
+            return result;
+        });
+};
+
+exports.createUser = (userData) => {
+    const user = new userModel(userData);
+    return user.save();
+};
+
+exports.list = (perPage, page) => {
+    return new Promise((resolve, reject) => {
+        userModel.find()
+            .limit(perPage)
+            .skip(perPage * page)
+            .exec(function (err, users) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(users);
+                }
+            })
+    });
+};
+
+exports.patchUser = (id, userData) => {
+    return userModel.findOneAndUpdate({
+        _id: id
+    }, userData);
+};
+
+exports.removeById = (userId) => {
+    return new Promise((resolve, reject) => {
+        userModel.deleteMany({_id: userId}, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(err);
+            }
+        });
+    });
+};
+
